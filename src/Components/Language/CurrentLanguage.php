2864dd263526779c31ab7e0302b6856fa8ee9aba
moroztaras <moroztaras@i.ua>
2018-06-24 11:00:03 +0300

    B9 setting Workflow

---
+++ src/Components/Language/CurrentLanguage.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moroztaras
+ * Date: 24.06.18
+ * Time: 9:47
+ */
+
+namespace App\Components\Language;
+
+
+class CurrentLanguage
+{
+
+}
---
+++ src/Entity/PageData.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: moroztaras
+ * Date: 24.06.18
+ * Time: 9:33
+ */
+
+namespace App\Entity;
+
+
+class PageData
+{
+
+}
